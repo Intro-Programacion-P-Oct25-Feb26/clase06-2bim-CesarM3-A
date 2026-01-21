573784af6bcc824e766285be67b6488e6c1da41c
CesarM3-A <cesarmateoab2007@gmail.com>
2026-01-21 11:56:44 -0500

    Ejemplo-051

--- EjemplosExcepciones/src/manejoexepciones/Ejemplo051.java
+++ EjemplosExcepciones/src/manejoexepciones/Ejemplo051.java
@@ -20,14 +20,19 @@ public class Ejemplo051 {
         while (bandera) {
             try {
                 Scanner entrada = new Scanner(System.in);
-                System.out.println("Ingrese valor 1:");
-                int valor1 = entrada.nextInt(); // se espera un valor entero 
+                System.out.println("Ingrese valor 1:"); 
+                int valor1 = entrada.nextInt(); // 20 - se espera un valor entero 
                 System.out.println("Ingrese valor 2:");
-                int valor2 = entrada.nextInt(); // se espera un valor entero // 0
-                int resultado = valor1 / valor2;
+                int valor2 = entrada.nextInt(); // 2 - se espera un valor entero // 0
+                int resultado = valor1 / valor2; // 20/2 >> 10
 
-                System.out.printf("Resultado %d\n", resultado);
+                System.out.printf("Resultado %d\n", resultado); // 10
                 bandera = false;
+                /*
+                Aqui en el codigo tenemos un ciclo While el cual nos va permitir
+                seguir dentro del ciclo hasta que el usuario ingrese bien los 
+                datos pedidos por teclado, sino este seguira repitiendose infinitamente.
+                */
             } catch (InputMismatchException inputMismatchException) {
 
                 System.out.printf("Existe un error de tipo %s\n",
